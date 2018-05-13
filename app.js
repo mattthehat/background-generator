1c0d637fcf04f1cfb4d02e8a25a470349e316a3d
mattTheHat <matthewrosser@matthews-imac.home>
2018-05-13 14:31:14 +0100

    Adding starter files

---
+++ app.js
@@ -0,0 +1,19 @@
+const css = document.querySelector('h3');
+const color1 = document.querySelector('#color1');
+const color2 = document.querySelector('#color2');
+const body = document.getElementById('gradient');
+
+console.log(css);
+console.log(color1);
+console.log(color2);
+console.log(body);
+
+changeBG = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = body.style.background;
+}
+
+color1.addEventListener("input", changeBG);
+color2.addEventListener("input", changeBG);
+
+
